2ec86e9658f40c62d601cad8dae796b4bd1994a7
chauhan-vishal <vkcvishal05@gmail.com>
2023-02-10 15:55:45 +0530

    mongoose-connection

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World!")
+const express = require("express")
+const app = express()
+
+const port = process.env.PORT || 2503
+app.listen(port, () => console.log(`App is listening on ${port}`))
+
+app.get("/api/cuisine/", (req, res) => {
+    res.send(req.url)
+})
